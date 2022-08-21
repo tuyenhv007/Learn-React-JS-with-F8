44ccaa7198c227c2428c880a7be1f1c63cbefbb2
Hoàng Văn Tuyến <tuyenhv@tienngay.vn>
2022-08-21 23:35:54 +0700

    Revert "mount and unmount component"
    This reverts commit 27f31fa15f6ab977f96b87210b6b25ae8d6ef081.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import Content from './Content';
 
 function App() {
-  const [show, setShow] = useState(false);
-  return(
-    <div style={{padding: 200}}>
-      <button 
-        onClick={() => setShow(!show)}
-      >
-        Toggle
-      </button>
-     {show &&  <Content />}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
