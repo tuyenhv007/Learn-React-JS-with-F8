27f31fa15f6ab977f96b87210b6b25ae8d6ef081
Hoàng Văn Tuyến <tuyenhv@tienngay.vn>
2022-08-21 22:52:00 +0700

    mount and unmount component

---
+++ src/Car.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Car extends React.Component {
+    render () {
+        return <h1>I'm a ginetic container!</h1>;
+    }
+}
+
+export default Car;
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+function Content() {
+    return(
+        <h1>
+            Học lập trình với F8!
+        </h1>
+    )
+}
+
+export default Content;
